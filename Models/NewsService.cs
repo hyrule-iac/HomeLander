a17d835bf7302b29aa4b49e7f567e6799ea1ea9c
admin <aldosan666@outlook.com>
2026-04-18 15:21:06 -0600

    Add project files.

---
+++ Models/NewsService.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+using static Kleva_Guide.Components.Pages.Home;
+
+namespace Kleva_Guide.Models
+{
+    public class NewsService
+    {
+        public async Task<List<NewsItem>> GetLiveNewsAsync()
+        {
+            var feedUrls = new[] {
+            "https://news.google.com/rss/search?q=cybersecurity+cve&hl=es-419",
+            "https://www.msn.com/es-mx/noticias/rss"
+        };
+
+            var items = new List<NewsItem>();
+
+            foreach (var url in feedUrls)
+            {
+                using var reader = XmlReader.Create(url);
+                var feed = SyndicationFeed.Load(reader);
+
+                items.AddRange(feed.Items.Take(5).Select(item => new NewsItem
+                {
+                    Title = item.Title.Text,
+                    Summary = item.Summary?.Text ?? "Click para leer más...",
+                    Source = feed.Title.Text,
+                    Category = url.Contains("google") ? "Security" : "Cloud",
+                    PublishDate = item.PublishDate.UtcDateTime == DateTime.MinValue
+                                  ? DateTime.UtcNow
+                                  : item.PublishDate.UtcDateTime,
+                    Link = item.Links.FirstOrDefault()?.Uri.ToString() ?? string.Empty
+                }));
+            }
+
+            return items;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Kleva_Guide.Components;
+using Kleva_Guide.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMemoryCache();
+// Add services to the container.
+builder.Services.AddScoped<NewsService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
